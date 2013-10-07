9554c42197bb405f16d85cc92abdb81c8442f20d
shadowman <shadowman@6440a0b7-bb9b-4666-a620-702d5202acbb>
2013-10-07 11:12:27 +0000

    Adde StringUtil.java

---
+++ java/lineage2/gameserver/utils/StringUtil.java
@@ -0,0 +1,10 @@
+package lineage2.gameserver.utils;
+
+public class StringUtil {
+
+	public static String concat(String string, String item, String string2) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
